58bc3fedf8ffffbcb7feb23014dd18397ef560ef
Grant Greer <viridiang93@gmail.com>
2022-07-18 01:03:17 -0700

    added digimon logo + api fetch logic

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import logo from './digimon-logo.jpeg';
+import { useState, useEffect } from "react";
 import './App.css';
 
 function App() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+ 
+  useEffect(() => {
+    fetch(`https://digimon-api.vercel.app/api/digimon`)
+      .then((response) => response.json())
+      .then((actualData) => { setData(actualData); setLoading(false); })
+      .catch((error) => { setError(error); setLoading(false); });
+      
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      
+      
     </div>
   );
 }
