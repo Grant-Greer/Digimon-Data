4f22a8a5c5323a917992be8485302bcad3978ec5
Grant Greer <viridiang93@gmail.com>
2022-08-18 05:00:11 -0700

    added typescript && reconfigured prettier settings

--- src/components/DigimonContainer.jsx
+++ src/components/DigimonContainer.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './styles/digimon-container.css';
 
 export default function DigimonContainer({ children }) {
   return <div className="digimon-container">{children}</div>;
 }
+
+DigimonContainer.propTypes = {
+  children: PropTypes.node.isRequired
+};
